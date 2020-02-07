83d05583629dd813613c094f39244187cdced4f6
silleknarf@gmail.com <silleknarf@gmail.com>
2020-02-07 01:07:15 +0000

    Implemented basic authentication using asp.NET identity

--- service/CopyToMeService/Startup.cs
+++ service/CopyToMeService/Startup.cs
@@ -26,23 +26,27 @@ namespace CopyToMeService
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseRouting();
+            app.UseStaticFiles();
+            app.UseAuthentication();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.UseRouting();
 
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapRazorPages();
             });
         }
     }
