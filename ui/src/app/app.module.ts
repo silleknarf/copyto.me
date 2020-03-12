1f8c601c0a80ea85faad12e4fea5103440793ed7
silleknarf@gmail.com <silleknarf@gmail.com>
2020-03-12 02:20:40 +0000

    WIP Adding subscribe button

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -12,6 +12,8 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { ApiAuthorizationModule } from 'src/api-authorization/api-authorization.module';
 import { AuthorizeGuard } from 'src/api-authorization/authorize.guard';
 import { AuthorizeInterceptor } from 'src/api-authorization/authorize.interceptor';
+import { SubscribeComponent } from './subscribe/subscribe.component';
+import { NgxPayPalModule } from '../../../../ngx-paypal/src/lib/ngx-paypal.module';
 
 @NgModule({
   declarations: [
@@ -19,17 +21,20 @@ import { AuthorizeInterceptor } from 'src/api-authorization/authorize.intercepto
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent
+    FetchDataComponent,
+    SubscribeComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
     ApiAuthorizationModule,
+    NgxPayPalModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent, canActivate: [AuthorizeGuard] },
+      { path: 'subscribe', component: SubscribeComponent, canActivate: [AuthorizeGuard] },
     ])
   ],
   providers: [
---
+++ ui/src/app/subscribe/subscribe.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { IPayPalConfig } from '../../../../../ngx-paypal/src/lib/models/paypal-models';
+
+@Component({
+  selector: 'app-subscribe',
+  templateUrl: './subscribe.component.html',
+  styleUrls: ['./subscribe.component.css']
+})
+export class SubscribeComponent implements OnInit {
+
+  public payPalConfig?: IPayPalConfig;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.initConfig();
+  }
+
+  private initConfig(): void {
+    this.payPalConfig = {
+        currency: 'USD',
+        clientId: 'sb',
+        createSubscription: (data, actions ) => {
+          return actions.subscription.create({
+            'plan_id': 'P-2UF78835G6983425GLSM44MA'
+          });
+        },
+        onApprove: (data, actions) => {
+          alert('You have successfully created subscription ' + data.subscriptionID);
+        },
+    };
+  }
+}
